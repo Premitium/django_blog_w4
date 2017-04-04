383c153214b68c435643db6b9d50fde0a54573ed
Premitium <simeonparvanov@ymail.com>
2017-04-04 12:19:09 +0300

    Added link for logo and edited url for index view

--- simpleblog/blog/urls.py
+++ simpleblog/blog/urls.py
@@ -4,7 +4,7 @@ from .views import index, create_blog_post, login_view, profile_view, single_pos
 
 
 urlpatterns = [
-    url(r'^$', index, name='index'),
+    url(r'^$|index', index, name='index'),
     url(r'^login/$', login_view, name='login'),
     url(r'^profile/$', profile_view, name='profile'),
     url(r'^create/$', create_blog_post, name='create'),
