abd8e5ecd8a7113ed7e78737e02146b57c03b65a
Premitium <simeonparvanov@ymail.com>
2017-04-07 19:20:03 +0300

    More tuning from heroku documentation

--- simpleblog/simpleblog/settings.py
+++ simpleblog/simpleblog/settings.py
@@ -28,6 +28,11 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 # Application definition
 
 INSTALLED_APPS = [
